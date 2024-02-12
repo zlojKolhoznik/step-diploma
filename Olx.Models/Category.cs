9ac540fbdce0b4545914ac427e78889e64fe9bc0
Roman Portianko <rportyanko10@gmail.com>
2024-02-12 12:57:01 +0200

    Initial commit

---
+++ Olx.Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Olx.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public int? ParentId { get; set; }
+
+    [ForeignKey(nameof(ParentId))]
+    public Category? Parent { get; set; }
+
+    public Category[]? Children { get; set; }
+
+    public string Name { get; set; }
+    
+    public string NormalizedName { get; set; }
+    
+    // TODO: Ask how to implement custom filters for each category.
+    // public Filters { get; set; }
+}
