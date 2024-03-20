9da1fb8856eb9d10690d8e90ed8f3091e515a312
Roman Portianko <rportyanko10@gmail.com>
2024-03-20 19:02:16 +0200

    Added EnterpriseUser.cs

---
+++ Olx/Models/EnterpriseUser.cs
@@ -0,0 +1,26 @@
+﻿namespace Olx.Models;
+
+public class EnterpriseUser
+{
+    public string CompanyName { get; set; }
+
+    public string StreetAddress { get; set; }
+
+    public string ZipCode { get; set; }
+    
+    public string CityAndRegion { get; set; }
+
+    public string RegistrationId { get; set; } // ЄДРПОУ
+    
+    public string TaxId { get; set; } // ІПН
+
+    public bool IsVatPayer { get; set; }
+
+    public string RepresenterName { get; set; }
+
+    public string ContactName { get; set; }
+    
+    public string ContactPhone { get; set; }
+    
+    public string ContactEmail { get; set; }
+}
--- Olx/Models/User.cs
+++ Olx/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace Olx.Models;
 
@@ -7,5 +8,8 @@ public class User : IdentityUser
 {
     public string? Name { get; set; }
 
-    public bool IsBusinessAccount { get; set; }
+    [ForeignKey(nameof(EnterpriseUser))]
+    public int? EnterpriseUserId { get; set; }
+    
+    public EnterpriseUser? EnterpriseUser { get; set; }
 }
