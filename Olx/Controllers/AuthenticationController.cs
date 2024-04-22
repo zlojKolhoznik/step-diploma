522eee34492a7f3cc040084beb4c3a86c455998d
Roman Portianko <rportyanko10@gmail.com>
2024-04-22 20:51:49 +0300

    Added login page

---
+++ Olx/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Olx.Models;
+using Olx.ViewModels;
+
+namespace Olx.Controllers;
+
+public class AuthenticationController : Controller
+{
+    private readonly SignInManager<User> _signInManager;
+    private readonly ILogger<AuthenticationController> _logger;
+
+    public AuthenticationController(SignInManager<User> signInManager, ILogger<AuthenticationController> logger)
+    {
+        _signInManager = signInManager;
+        _logger = logger;
+    }
+    
+    [HttpGet]
+    public IActionResult Login(string returnUrl = "/")
+    {
+        return View(new LoginViewModel { ReturnUrl = returnUrl });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(string login, string password, bool isPersistent, string returnUrl = "/")
+    {
+        var result = await _signInManager.PasswordSignInAsync(login, password, isPersistent, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+            _logger.LogInformation("User logged in.");
+            return LocalRedirect(returnUrl);
+        }
+
+        return LocalRedirect($"{returnUrl}?error=Invalid login attempt&login"); // If login fails, redirect back to the login page with an error message
+    }
+}
---
+++ Olx/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Olx.ViewModels;
+
+public class LoginViewModel
+{
+    [Display(Name = "Електронна пошта")]
+    public string? Username { get; set; }
+
+    [Display(Name = "Пароль")]
+    public string? Password { get; set; }
+    
+    [Display(Name = "Запам'ятати мене")]
+    public bool IsPersistent { get; set; }
+    
+    public string? ReturnUrl { get; set; }
+    
+    public string? ErrorMessage { get; set; }
+}
